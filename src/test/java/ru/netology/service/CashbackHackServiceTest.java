968672b3eeca868247a7a3022b831a15d6c95cea
Grisha Petrosyan <petrosyan.grigorij2018@yandex.ru>
2023-03-25 20:34:36 +0300

    Gitignoreeeeeeed adddedddd

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
@@ -10,7 +11,7 @@ public class CashbackHackServiceTest {
         int amount = 5700;
         int actual = service.remain(amount);
         int expected = 300;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -18,7 +19,7 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,9 +27,11 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
+
 }
+
 
 
 
